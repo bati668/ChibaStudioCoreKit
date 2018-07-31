0a11fbe74b6248bda6aff9312f2afa54f549758b
bati668 <bati@hiroshi.in>
2018-07-31 22:40:49 +0900

    Initial Commit

---
+++ ChibaStudioCoreKit/ChibaStudioCoreKit.h
@@ -0,0 +1,19 @@
+//
+//  ChibaStudioCoreKit.h
+//  ChibaStudioCoreKit
+//
+//  Created by 千葉大志 on 2018/07/31.
+//  Copyright © 2018年 Chiba factory. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChibaStudioCoreKit.
+FOUNDATION_EXPORT double ChibaStudioCoreKitVersionNumber;
+
+//! Project version string for ChibaStudioCoreKit.
+FOUNDATION_EXPORT const unsigned char ChibaStudioCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChibaStudioCoreKit/PublicHeader.h>
+
+
